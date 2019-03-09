d122f3f81025bdb318ade83d03d625eb4c8b7c49
Diego Gorgazzi <44508750+DiegoGorgazzi@users.noreply.github.com>
2019-03-09 14:47:40 +0100

    Import QuoteBox component to App container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import QuoteBox from "./components/QuoteBox/QuoteBox";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
 
+      <h1>hello </h1>
+      <QuoteBox />
 
       </div>
     );
