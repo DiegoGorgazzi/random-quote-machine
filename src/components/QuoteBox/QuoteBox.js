64a59be7ead791a420ec7246103d7d66ae8bd8c1
Diego Gorgazzi <44508750+DiegoGorgazzi@users.noreply.github.com>
2019-03-09 17:09:09 +0100

    Add ids required
    Project requires some elements to have specific id names

--- src/components/QuoteBox/QuoteBox.js
+++ src/components/QuoteBox/QuoteBox.js
@@ -2,7 +2,25 @@ import React from "react";
 
 const quoteBox = () => {
     return (
-      <p> To be or Not to be, that is the question </p>
+      <div id="quote-box">
+
+        <div id="text">
+          <p> To be or Not to be, that is the question </p>
+        </div>
+
+        <div id="author">
+            <p>Author</p>
+        </div>
+
+        <div id="tweet-quote">
+            <a href="https://twitter.com/intent/tweet">Tweeter</a>
+        </div>
+
+        <button id="new-quote"> New quote </button>
+
+
+      </div>
+
     )
 };
 
