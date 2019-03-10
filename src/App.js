d73fb485c3a984b925220d92991983fcd8133fbb
Diego Gorgazzi <44508750+DiegoGorgazzi@users.noreply.github.com>
2019-03-10 12:18:58 +0100

    Add random functionality to componentDidMount

--- src/App.js
+++ src/App.js
@@ -24,19 +24,27 @@ class App extends Component {
               qAuthor: "Warren Buffet"},
           {qId:  4,
             qText: "If you're going through hell, keep going",
-              qAuthor: "Winston Churchill"}
+              qAuthor: "Winston Churchill"},
+          {qId:  5,
+              qText: "These things I have spoken to you that in Me you may have apeace. In the world you have baffliction, but take courage; I have covercome the world.",
+                qAuthor: "John 16:33 (The Bible, Recovery Version)"},
+          {qId:  6,
+              qText: "Our greatest glory is not in never falling, but in rising every time we fall.",
+                qAuthor: "Confucius"},
+          {qId:  7,
+              qText: "I learned that courage was not the absence of fear, but the triumph over it. The brave man is not he who does not feel afraid, but he who conquers that fear",
+                qAuthor: "Nelson Mandela"},
+          {qId:  8,
+              qText: "When the whole world is silent, even one voice becomes powerful.",
+                qAuthor: "Malala Yousafzai"},
+          {qId:  9,
+                qText: "“If you don’t like the road you’re walking, start paving another one.",
+                qAuthor: "Dolly Parton"}
         ];
 
+      this.setState ({
+         quotes: quotesList[Math.floor((Math.random() * (quotesList.length)))]
 
-       this.setState ({
-         quotes: quotesList[3]
-         /*
-           quotes: {
-             qId:  2,
-             qText: "To be or Not to be. That is the question." ,
-             qAuthor: "shakespeare"
-          }
-          */
        })
 
        //console.log(quotesList[3]);
@@ -44,20 +52,9 @@ class App extends Component {
      }
 
 
-//I shouldn't use, 1, 2, 3... I should use index instead so that
-//if new quotes are added, I don't have to change anything.
-//Although I would still have to set the qId / key to index
+//I still have to set the qId / key to index
 //I need a setState somewhere inside a handler for the button
 //don't forget about the button eventually.
-/* I need to figure out where/how to put list of quotes that I'll
-eventually use to update the state
-const quotesList = [
-    {qId:  1, qText: "Better safe than Sorry"   ,  qAuthor: "Anonymous"},
-    {qId:  2, qText: "To be or Not to be. That is the question." ,  qAuthor: "shakespeare"},
-    {qId:  3, qText: "Be greedy when others are fearful; be fearful when others are greedy, qAuthor: "Warren Buffet"},
-    {qId:  4, qText: "If you're going through hell, keep going", qAuthor: "Winston Churchill"}
-  ];
-*/
 
   render() {
 
